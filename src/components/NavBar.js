4a860db70fc110f118698a91b2ababf65baf0155
Maria Jenkins <macjenkin@gmail.com>
2023-08-01 14:20:31 -0500

    add navbar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,37 @@
 import React from "react"
 import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 
+const linkStyles = {
+    display: "inline-block",
+    width: "50px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "green",
+    textDecoration: "none",
+    color: "white",
+};
+
 function NavBar() {
     return (
         <div>
-            <NavLink to="/" exact >
+            <NavLink 
+                to="/" 
+                exact
+                style={linkStyles}
+                activeStyle={{
+                  background: "darkgreen",
+                }}
+            >
                 Home
             </NavLink>
-            <NavLink to="/deck" exact >
+            <NavLink 
+                to="/deck" 
+                exact
+                style={linkStyles}
+                activeStyle={{
+                  background: "darkgreen",
+                }}
+            >
                 Deck
             </NavLink>
         </div>
