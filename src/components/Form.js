9f4fc47bb65acbf50061efc9925db1be1d8b73ce
Maria Jenkins <macjenkin@gmail.com>
2023-08-01 17:20:15 -0500

    set up form basics

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Form() {
+    const [formData, setFormData] = useState({
+        name: "",
+        image: "",
+        type: "",
+        colors: [],
+        main: false
+    })
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log(formData)
+        setFormData({
+            name: "",
+            image: "",
+            type: "",
+            colors: [],
+            main: false
+        })
+    }
+
+    function handleChange(e) {
+        const key = e.target.id
+        const value = e.target.value
+
+        setFormData({
+            ...formData,
+            [key]: value
+        })
+    }
+
     return (
         <div>
-            Form
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Name</label>
+                <input 
+                    type="text"
+                    id="name"
+                    placeholder="Type card name here"
+                    value={formData.name}
+                    onChange={e => handleChange(e)}
+                    required
+                />
+                <label htmlFor="image">Image</label>
+                <input 
+                    type="text"
+                    id="image"
+                    placeholder="Type image url here"
+                    value={formData.image}
+                    onChange={e => handleChange(e)}
+                    required
+                />
+                <input type="submit" value="Submit Card" />
+            </form>
         </div>
     )
 }
