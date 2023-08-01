338dbd99d1e48ef6a2e78b63dc0b52199be784b6
Maria Jenkins <macjenkin@gmail.com>
2023-08-01 18:29:01 -0500

    add form elements

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import SelectColors from "./SelectColors";
 
 function Form() {
     const [formData, setFormData] = useState({
@@ -8,6 +9,7 @@ function Form() {
         colors: [],
         main: false
     })
+    const [selectedColors, setSelectedColors] = useState([])
 
     function handleSubmit(e) {
         e.preventDefault()
@@ -19,6 +21,7 @@ function Form() {
             colors: [],
             main: false
         })
+        setSelectedColors([])
     }
 
     function handleChange(e) {
@@ -29,6 +32,14 @@ function Form() {
             ...formData,
             [key]: value
         })
+    }
+
+    function handleColorChange(colors) {
+        setFormData({
+            ...formData,
+            colors: colors.map(option => option.value)
+        })
+        setSelectedColors(colors)
     }
 
     return (
@@ -52,6 +63,24 @@ function Form() {
                     onChange={e => handleChange(e)}
                     required
                 />
+                <label htmlFor="type">Type</label>
+                <select 
+                    id="type"
+                    value={formData.type} 
+                    onChange={e => handleChange(e)}
+                    required
+                >
+                    <option value="" disabled>Select Card Type</option>
+                    <option value="creature">Creature</option>
+                    <option value="sorcery">Sorcery</option>
+                    <option value="instant">Instant</option>
+                    <option value="artifact">Artifact</option>
+                    <option value="enchantment">Enchantment</option>
+                    <option value="land">Land</option>
+                </select>
+                <label htmlFor="colors">Colors</label>
+                <SelectColors id="colors" onHandleColorChange={handleColorChange} selectedColors={selectedColors} />
+
                 <input type="submit" value="Submit Card" />
             </form>
         </div>
---
+++ src/components/SelectColors.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Select from "react-select"
+
+function SelectColors({ selectedColors, onHandleColorChange }) {
+    return(
+        <Select 
+            isMulti
+            name="colors"
+            value={selectedColors}
+            options={[
+                { value: 'black', label: 'Black', color: 'black' },
+                { value: 'blue', label: 'Blue', color: 'blue' },
+                { value: 'green', label: 'Green', color: 'green' },
+                { value: 'red', label: 'Red', color: 'red' },
+                { value: 'white', label: 'White', color: 'white' }
+            ]}
+            className="basic-multi-select"
+            classNamePrefix="select"
+            onChange={e => onHandleColorChange(e)}
+        />
+    )
+}
+
+export default SelectColors
