3bb3682650a2f30f249a99faada6c4b4cdaf3690
Maria Jenkins <macjenkin@gmail.com>
2023-08-02 01:31:04 -0500

    add deck title

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -4,39 +4,45 @@ import Container from "./Container"
 function Deck({ cards, onAddOrRemove }) {
     return (
         <div id="deck">
+            <div id="deckheader">
+                <span>
+                    <h1 className="decktitle">My Deck</h1></span>
+                <span><h1>Total Cards — {cards.length}</h1></span>                
+            </div>
+
             <div className="sectionheader">
-                <span><h1>Creatures</h1></span>
-                <span><h1 className="floatright">{cards.filter(card => card.type === "creature").length}</h1></span>                
+                <span><h2>Creatures</h2></span>
+                <span><h2>{cards.filter(card => card.type === "creature").length}</h2></span>                
             </div>
             <Container cards={cards.filter(card => card.type === "creature")} onAddOrRemove={onAddOrRemove} />
 
             <div className="sectionheader">
-                <span><h1>Sorceries</h1></span>
-                <span><h1 className="floatright">{cards.filter(card => card.type === "sorcery").length}</h1></span>                
+                <span><h2>Sorceries</h2></span>
+                <span><h2>{cards.filter(card => card.type === "sorcery").length}</h2></span>                
             </div>
             <Container cards={cards.filter(card => card.type === "sorcery")} onAddOrRemove={onAddOrRemove} />
 
             <div className="sectionheader">
-                <span><h1>Instants</h1></span>
-                <span><h1 className="floatright">{cards.filter(card => card.type === "instant").length}</h1></span>                
+                <span><h2>Instants</h2></span>
+                <span><h2>{cards.filter(card => card.type === "instant").length}</h2></span>                
             </div>
             <Container cards={cards.filter(card => card.type === "instant")} onAddOrRemove={onAddOrRemove} />
 
             <div className="sectionheader">
-                <span><h1>Artifacts</h1></span>
-                <span><h1 className="floatright">{cards.filter(card => card.type === "artifact").length}</h1></span>                
+                <span><h2>Artifacts</h2></span>
+                <span><h2>{cards.filter(card => card.type === "artifact").length}</h2></span>                
             </div>
             <Container cards={cards.filter(card => card.type === "artifact")} onAddOrRemove={onAddOrRemove} />
 
             <div className="sectionheader">
-                <span><h1>Enchantments</h1></span>
-                <span><h1 className="floatright">{cards.filter(card => card.type === "enchantment").length}</h1></span>                
+                <span><h2>Enchantments</h2></span>
+                <span><h2>{cards.filter(card => card.type === "enchantment").length}</h2></span>                
             </div>
             <Container cards={cards.filter(card => card.type === "enchantment")} onAddOrRemove={onAddOrRemove} />
 
             <div className="sectionheader">
-                <span><h1>Lands</h1></span>
-                <span><h1 className="floatright">{cards.filter(card => card.type === "land").length}</h1></span>                
+                <span><h2>Lands</h2></span>
+                <span><h2>{cards.filter(card => card.type === "land").length}</h2></span>                
             </div>
             <Container cards={cards.filter(card => card.type === "land")} onAddOrRemove={onAddOrRemove} />
         </div>
