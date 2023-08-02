5b0e6d3f78efa8aa035cbc19805900de630aa516
Maria Jenkins <macjenkin@gmail.com>
2023-08-02 00:42:38 -0500

    deck section header styling and count

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -4,22 +4,40 @@ import Container from "./Container"
 function Deck({ cards, onAddOrRemove }) {
     return (
         <div id="deck">
-            <h1>Creatures</h1>
+            <div className="sectionheader">
+                <span><h1>Creatures</h1></span>
+                <span><h1 className="floatright">{cards.filter(card => card.type === "creature").length}</h1></span>                
+            </div>
             <Container cards={cards.filter(card => card.type === "creature")} onAddOrRemove={onAddOrRemove} />
 
-            <h1>Sorceries</h1>
+            <div className="sectionheader">
+                <span><h1>Sorceries</h1></span>
+                <span><h1 className="floatright">{cards.filter(card => card.type === "sorcery").length}</h1></span>                
+            </div>
             <Container cards={cards.filter(card => card.type === "sorcery")} onAddOrRemove={onAddOrRemove} />
 
-            <h1>Instants</h1>
+            <div className="sectionheader">
+                <span><h1>Instants</h1></span>
+                <span><h1 className="floatright">{cards.filter(card => card.type === "instant").length}</h1></span>                
+            </div>
             <Container cards={cards.filter(card => card.type === "instant")} onAddOrRemove={onAddOrRemove} />
 
-            <h1>Artifacts</h1>
+            <div className="sectionheader">
+                <span><h1>Artifacts</h1></span>
+                <span><h1 className="floatright">{cards.filter(card => card.type === "artifact").length}</h1></span>                
+            </div>
             <Container cards={cards.filter(card => card.type === "artifact")} onAddOrRemove={onAddOrRemove} />
 
-            <h1>Enchantments</h1>
+            <div className="sectionheader">
+                <span><h1>Enchantments</h1></span>
+                <span><h1 className="floatright">{cards.filter(card => card.type === "enchantment").length}</h1></span>                
+            </div>
             <Container cards={cards.filter(card => card.type === "enchantment")} onAddOrRemove={onAddOrRemove} />
 
-            <h1>Lands</h1>
+            <div className="sectionheader">
+                <span><h1>Lands</h1></span>
+                <span><h1 className="floatright">{cards.filter(card => card.type === "land").length}</h1></span>                
+            </div>
             <Container cards={cards.filter(card => card.type === "land")} onAddOrRemove={onAddOrRemove} />
         </div>
     )
